8856148ce59697f3fe4ba179e7e10556b3aff2af
Edwin Benitez <edwinbenitez3216@gmail.com>
2019-10-09 23:34:11 -0400

    Fixed color issue for artifact

--- artifact.js
+++
@@ -1,6 +0,0 @@
-var fullCommand = msg.content;
-
-var globalName = {
-    
-   aname : fullCommand.substr(fullCommand.indexOf(' ') + 1, fullCommand.indexOf(' ') + 2)
-};
--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const Discord = require('discord.js');
 require('dotenv').config();
 const bot = new Discord.Client();
 const axios = require('axios');
-<<<<<<< HEAD
-const token = process.env.TOKEN;
-=======
 const token = process.env.BOT_TOKEN;
->>>>>>> f9e52384f5b79fb2396d0ccb73dcf3ae6210bab6
 
 bot.on('ready', () =>{
 	console.log('This bot is online');
@@ -33,20 +29,17 @@ bot.on('message', msg=>{
   .finally(function () {
     // always executed
   });
-<<<<<<< HEAD
   }
 
   if("artifact" === command){
     axios.get('https://api.epicsevendb.com/api/artifact/' + name)
   .then(function (response) {
     // handle success
-    var fullData = response.data.results[0];
+    var exclusive = response.data.results[0].exclusive[0];
     var dataBase = response.data.results[0].skillDescription.base;
     var dataMax = response.data.results[0].skillDescription.max;
     var fullSkill = attribute === 'skillDescription' ? fullData.skillDescription : '';
-    var exclusive = fullData.exclusive;
-    console.log(fullData);
-    msg.channel.send(getArtifactStats(name, exclusive, fullSkill, dataBase, dataMax));
+    msg.channel.send(getArtifactStats(name, exclusive, dataBase, dataMax));
   })
   .catch(function (error) {
     // handle error
@@ -56,9 +49,6 @@ bot.on('message', msg=>{
     // always executed
   });
   }
-=======
-	}
->>>>>>> f9e52384f5b79fb2396d0ccb73dcf3ae6210bab6
 })
 
 function getAllStats(hero, element, data){
@@ -83,7 +73,6 @@ function getAllStats(hero, element, data){
       color = "RANDOM";
   }
   var embed = new Discord.RichEmbed()
-<<<<<<< HEAD
   .addField("Level 50 Full Star Awaken", "Stats")
   .addField("CP", data.lv50FiveStarFullyAwakened.cp, true)
   .addField("ATK", data.lv50FiveStarFullyAwakened.atk, true)
@@ -106,58 +95,31 @@ function getAllStats(hero, element, data){
   .addField("EFF", data.lv60SixStarFullyAwakened.eff, true)
   .addField("EFR", data.lv60SixStarFullyAwakened.efr, true)
   .addField("DAC", data.lv60SixStarFullyAwakened.dac, true)
-=======
-  .addField("Level 1 No Star Awaken", "Stats")
-  .addField("COMBAT POWER", data.lv1BaseStarNoAwaken.cp, true)
-  .addField("ATTACK", data.lv1BaseStarNoAwaken.atk, true)
-  .addField("HP", data.lv1BaseStarNoAwaken.hp, true)
-  .addField("SPEED", data.lv1BaseStarNoAwaken.spd, true)
-  .addField("DEFENSE", data.lv1BaseStarNoAwaken.def, true)
-  .addField("CRIT CHANCE", data.lv1BaseStarNoAwaken.chc, true)
-  .addField("CRIT DAMAGE", data.lv1BaseStarNoAwaken.chd, true)
-  .addField("EFFECTIVENESS", data.lv1BaseStarNoAwaken.eff, true)
-  .addField("EFFECT RESISTANCE", data.lv1BaseStarNoAwaken.efr, true)
-  .addField("DUAL ATTACK CHANCE", data.lv1BaseStarNoAwaken.dac, true)
-  .addBlankField()
-  .addField("Level 60 Full Star Awaken", "Stats")
-  .addField("CP", data.lv60SixStarFullyAwakened.cp, true)
-  .addField("ATTACK", data.lv60SixStarFullyAwakened.atk, true)
-  .addField("HP", data.lv60SixStarFullyAwakened.hp, true)
-  .addField("SPEED", data.lv60SixStarFullyAwakened.spd, true)
-  .addField("DEFENSE", data.lv60SixStarFullyAwakened.def, true)
-  .addField("CRIT CHANCE", data.lv60SixStarFullyAwakened.chc, true)
-  .addField("CRIT DAMAGE", data.lv60SixStarFullyAwakened.chd, true)
-  .addField("EFFECTIVENESS", data.lv60SixStarFullyAwakened.eff, true)
-  .addField("EFFECT RESISTANCE", data.lv60SixStarFullyAwakened.efr, true)
-  .addField("DUAL ATTACK CHANCE", data.lv60SixStarFullyAwakened.dac, true)
->>>>>>> f9e52384f5b79fb2396d0ccb73dcf3ae6210bab6
   .setColor(color)
   .attachFile('https://assets.epicsevendb.com/hero/' + hero + '/full.png');
   return embed;
 }
-<<<<<<< HEAD
 
 
-function getArtifactStats(artifact, exclusive, data, base, max){
-  var color = "RANDOM";
+function getArtifactStats(artifact, exclusive, base, max){
   switch (exclusive){
     case "mage":
       color = "BLUE";
       break;
     case "soul-weaver":
-      color = "BLUE";
-      break;
-    case "knight":
       color = "GREEN";
       break;
+    case "knight":
+      color = "DARK_BLUE";
+      break;
     case "warrior":
-      color = "GOLD";
+      color = "RED";
       break;
     case "theif":
       color = "PURPLE";
       break;
     case "ranger":
-      color = "GREEN";
+      color = "DARK_GREEN";
       break;
     case "common":
       color = "ORANGE";
@@ -174,7 +136,5 @@ function getArtifactStats(artifact, exclusive, data, base, max){
   .attachFile('https://assets.epicsevendb.com/artifact/' + artifact + '/full.png');
   return embed;
 }
-=======
->>>>>>> f9e52384f5b79fb2396d0ccb73dcf3ae6210bab6
 
 bot.login(token);
